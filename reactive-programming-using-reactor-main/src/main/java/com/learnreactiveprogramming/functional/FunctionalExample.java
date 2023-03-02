71258c4d4d9935895f31e41286524b7fd424165d
Christian <christiancatamuscay@r-fast.com>
2023-03-01 21:11:09 -0500

    11. Imperative Style vs Functional Style(Sección 6: Functional Programming in Modern Java)

--- reactive-programming-using-reactor-main/src/main/java/com/learnreactiveprogramming/functional/FunctionalExample.java
+++ reactive-programming-using-reactor-main/src/main/java/com/learnreactiveprogramming/functional/FunctionalExample.java
@@ -1,4 +1,20 @@
 package com.learnreactiveprogramming.functional;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class FunctionalExample {
+    public static void main(String[] args) {
+        var namesList = List.of("alex","ben","chole","adam");
+        var newNamesList = namesGreaterThanSize(namesList, 3);
+        System.out.println("newNameList : "+newNamesList);
+    }
+
+    private static List<String> namesGreaterThanSize(List<String> namesList, int i) {
+        return  namesList.stream()
+                .filter(s->s.length() >3)
+                .map(String::toUpperCase)
+                .distinct()
+                .collect(Collectors.toList());
+    }
 }
--- reactive-programming-using-reactor-main/src/main/java/com/learnreactiveprogramming/imperative/ImperativeExample.java
+++ reactive-programming-using-reactor-main/src/main/java/com/learnreactiveprogramming/imperative/ImperativeExample.java
@@ -1,4 +1,26 @@
 package com.learnreactiveprogramming.imperative;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class ImperativeExample {
+
+    public static void main(String[] args) {
+        var namesList = List.of("alex","ben","chole","adam");
+        var newNamesList = namesGreaterThanSize(namesList, 3);
+        System.out.println("newNameList : "+newNamesList);
+    }
+
+    private static List<String> namesGreaterThanSize(List<String> namesList, int i) {
+        List<String> newNameList = new ArrayList<>();
+        for (String name: namesList) {
+            if(name.length() > 3 && !newNameList.contains(name) ){
+                newNameList.add(name.toUpperCase());
+            }
+        }
+        return newNameList;
+    }
+
+
 }
