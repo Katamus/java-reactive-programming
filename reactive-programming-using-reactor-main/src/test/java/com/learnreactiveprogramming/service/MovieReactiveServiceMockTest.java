432e9a6b4ac427b918cfb608bc198977dc795279
Christian <christiancatamuscay@r-fast.com>
2023-03-05 18:42:13 -0500

    45. Test Exception in MoviesReactiveService using Mockito(Sección 13: Implement Exception Handling in Movies Reactive Service)

---
+++ reactive-programming-using-reactor-main/src/test/java/com/learnreactiveprogramming/service/MovieReactiveServiceMockTest.java
@@ -0,0 +1,66 @@
+package com.learnreactiveprogramming.service;
+
+import com.learnreactiveprogramming.exception.MovieException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.test.StepVerifier;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+
+@ExtendWith(MockitoExtension.class)
+class MovieReactiveServiceMockTest {
+
+    @Mock
+    private MovieInfoService movieInfoService;
+
+
+    @Mock
+    private ReviewService reviewService;
+
+    @InjectMocks
+    MovieReactiveService reactiveMovieService;
+
+    @InjectMocks
+    MovieReactiveService movieReactiveService;
+
+    @Test
+    void getAllMovies(){
+
+        Mockito.when(movieInfoService.retrieveMoviesFlux()).thenCallRealMethod();
+
+        Mockito.when(reviewService.retrieveReviewsFlux(anyLong()))
+                .thenCallRealMethod();
+
+        var moviesFlux = reactiveMovieService.getAllMovies();
+
+        StepVerifier.create(moviesFlux).expectNextCount(3).verifyComplete();
+
+
+    }
+
+    @Test
+    void getAllMovies_1(){
+
+        var erroMessage = "Exception occurred in ReviewService";
+
+        Mockito.when(movieInfoService.retrieveMoviesFlux()).thenCallRealMethod();
+
+        Mockito.when(reviewService.retrieveReviewsFlux(anyLong())).thenThrow(new RuntimeException(erroMessage));
+
+        var moviesFlux = reactiveMovieService.getAllMovies();
+
+        StepVerifier.create(moviesFlux)
+                //.expectError(MovieException.class)
+                .expectErrorMessage(erroMessage)
+                .verify();
+
+
+
+    }
+
+}
