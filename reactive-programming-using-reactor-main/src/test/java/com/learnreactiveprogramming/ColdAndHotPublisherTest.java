a5de30644747e3cb19d3b2f4cf6a331b4c467815
Christian <christiancatamuscay@r-fast.com>
2023-03-09 14:35:31 -0500

    63. Cold Streams(Sección 19: Cold & Hot Streams)

--- reactive-programming-using-reactor-main/src/test/java/com/learnreactiveprogramming/ColdAndHotPublisherTest.java
+++ reactive-programming-using-reactor-main/src/test/java/com/learnreactiveprogramming/ColdAndHotPublisherTest.java
@@ -1,4 +1,16 @@
 package com.learnreactiveprogramming;
 
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+
 public class ColdAndHotPublisherTest {
+
+
+    @Test
+    void coldPlubliserTest(){
+        var flux = Flux.range(1,10);
+        flux.subscribe(x-> System.out.println("Subscriber 1 : " + x));
+        flux.subscribe(y-> System.out.println("Subscriber 2 : " + y));
+    }
+
 }
