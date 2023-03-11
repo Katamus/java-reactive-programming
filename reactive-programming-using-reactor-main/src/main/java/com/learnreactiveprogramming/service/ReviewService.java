4c29829b2e40913dcf6b162f1550e123fcbecc36
Christian <christiancatamuscay@r-fast.com>
2023-03-11 15:56:18 -0500

    71. Build the non-blocking Review RestClient(Sección 21: Build NonBlocking RestClient using WebClient)

--- reactive-programming-using-reactor-main/src/main/java/com/learnreactiveprogramming/service/ReviewService.java
+++ reactive-programming-using-reactor-main/src/main/java/com/learnreactiveprogramming/service/ReviewService.java
@@ -1,11 +1,37 @@
 package com.learnreactiveprogramming.service;
 
 import com.learnreactiveprogramming.domain.Review;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Flux;
 
 import java.util.List;
 
 public class ReviewService {
+
+    WebClient webClient;
+
+    public ReviewService(WebClient webClient) {
+        this.webClient = webClient;
+    }
+
+    public ReviewService() {
+
+    }
+
+    public Flux<Review> retrieveReviewsFlux_RestCliente(Long movieInfoId){
+        var uri = UriComponentsBuilder.fromUriString("/v1/reviews")
+                .queryParam("movieInfoId",movieInfoId)
+                .buildAndExpand()
+                .toUriString();
+
+        return webClient.get().uri(uri)
+                .retrieve()
+                .bodyToFlux(Review.class)
+                .log();
+
+    }
 
     public  List<Review> retrieveReviews(long movieInfoId){
 
