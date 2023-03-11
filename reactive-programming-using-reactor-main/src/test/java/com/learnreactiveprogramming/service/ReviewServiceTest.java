b512047fd29a0b9b2f5f8fe3d0568dc6c24f71e3
Christian <christiancatamuscay@r-fast.com>
2023-03-11 15:57:22 -0500

    Tarea 10: Write a test case for retrieveReviewsFlux_RestClient function

--- reactive-programming-using-reactor-main/src/test/java/com/learnreactiveprogramming/service/ReviewServiceTest.java
+++ reactive-programming-using-reactor-main/src/test/java/com/learnreactiveprogramming/service/ReviewServiceTest.java
@@ -1,4 +1,30 @@
+package com.learnreactiveprogramming.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.test.StepVerifier;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ReviewServiceTest {
-  
+
+    WebClient webClient = WebClient.builder()
+            .baseUrl("http://localhost:8080/movies")
+            .build();
+
+    ReviewService reviewService = new ReviewService(webClient);
+
+    @Test
+    void retrieveReviewsFlux_RestCliente() {
+
+        var fluxReview = reviewService.retrieveReviewsFlux_RestCliente(1L).doOnNext(review -> {
+            System.out.println(review.getReviewId());
+        }).log();
+
+        StepVerifier.create(fluxReview)
+                .assertNext(
+                        review -> assertEquals("Nolan is the real superhero",review.getComment())
+                ).verifyComplete();
+    }
+
 }
