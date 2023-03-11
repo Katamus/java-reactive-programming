8478c174791ed9a1cab51d7567a4af9c8eb1a15f
Christian <christiancatamuscay@r-fast.com>
2023-03-11 09:48:38 -0500

    64. Hot Streams - ConnectableFlux(Sección 19: Cold & Hot Streams)

--- reactive-programming-using-reactor-main/src/test/java/com/learnreactiveprogramming/ColdAndHotPublisherTest.java
+++ reactive-programming-using-reactor-main/src/test/java/com/learnreactiveprogramming/ColdAndHotPublisherTest.java
@@ -1,7 +1,12 @@
 package com.learnreactiveprogramming;
 
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.ConnectableFlux;
 import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+
+import static com.learnreactiveprogramming.util.CommonUtil.delay;
 
 public class ColdAndHotPublisherTest {
 
@@ -11,6 +16,20 @@ public class ColdAndHotPublisherTest {
         var flux = Flux.range(1,10);
         flux.subscribe(x-> System.out.println("Subscriber 1 : " + x));
         flux.subscribe(y-> System.out.println("Subscriber 2 : " + y));
+    }
+
+    @Test
+    void gotPlubliserTest(){
+        var flux = Flux.range(1,10)
+                .delayElements(Duration.ofSeconds(1));
+
+        ConnectableFlux<Integer> connectableFlux = flux.publish();
+        connectableFlux.connect();
+
+        connectableFlux.subscribe(x-> System.out.println("Subscriber 1 : " + x));
+        delay(4000);
+        connectableFlux.subscribe(x-> System.out.println("Subscriber 2 : " + x));
+        delay(10000);
     }
 
 }
